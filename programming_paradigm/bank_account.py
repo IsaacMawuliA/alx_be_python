8b1ba9e973492e8569fe197f0fbb7b5d9e190293
IsaacMawuliA <m.want35@gmail.com>
2024-10-08 11:02:53 -0700

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,19 +4,19 @@ class BankAccount:
     def __init__(self, initial_balance=0):
         self.__account_balance = initial_balance
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited: ${amount:.2f}")
-        else:
-            print("Deposit amount must be positive.")
+def deposit(self, amount):
+    if amount <= 0:
+        print("Invalid amount.")
+    else:
+        self.__account_balance += amount
+        print(f"Deposited: ${amount:.2f}")
 
     def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
-            print("Insufficient funds or invalid amount.")
+            print("Insufficient funds.")
             return False
 
     def display_balance(self):
