c103d96083082531b9111c91ec98a2af02db5fa2
IsaacMawuliA <m.want35@gmail.com>
2024-10-08 07:57:55 -0700

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,14 +13,14 @@ class BankAccount:
     def withdraw(self, amount):
         if amount > 0 and amount <= self._balance:
             self._balance -= amount
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.1f}")
             return True
         elif amount > 0:
             print("Insufficient funds.")
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self._balance:.2f}")
+        print(f"Current Balance: ${self._balance:.1f}")
 
 
 # main-0.py
