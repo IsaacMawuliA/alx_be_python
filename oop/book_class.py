ea5a12ae0caafccb246f341240b6dc1178ecac7c
IsaacMawuliA <m.want35@gmail.com>
2024-10-08 18:06:50 -0700

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,6 +12,6 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book(title='{self.title}', author='{self.author}', year={self.year})"
+    return f"Book('{self.title}', '{self.author}', {self.year})"
 
 
